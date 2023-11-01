48861f8b811e056c5a4b0fe4d1af26b3b6260419
Fred Bahr <flbahr@mbari.org>
2023-11-01 12:19:25 -0700

    Add files via upload
    Adds the anom and avmap structures to the map structure.  The difference between anomamp2map and anommap2map_woa are the optional variables included.  WOA only has t and s.

---
+++ anommap2map_woa.m
@@ -0,0 +1,37 @@
+function map = anommap2map_woa(anommap,avmap)
+
+% Takes 'anommap' and 'avmap' structures to compute the total t, s, u, v 
+% fields and puts them in 'map' structure.
+%
+% K.Zaba; Aug 5, 2016
+%
+% modified to have ancillary variables beyond what Dan had.
+% The code just adds to a map structure.
+% FLB Oct. 24 2023.
+
+map.config = anommap.config;
+map.line   = anommap.line;
+map.depth  = anommap.depth;
+map.dist   = anommap.dist;
+map.lat    = anommap.lat;
+map.lon    = anommap.lon;
+map.time   = anommap.time;
+
+iiav = 1:10:length(avmap.time);
+nrep = length(map.time)/length(avmap.time(iiav));
+%vars = {'t','s','u','v','fl'}; % original
+%vars = {'t','s','u','v','fl','ox','cdom','bb'}; % new may need to remove u,v
+vars={'t','s'};
+for k=1:length(vars)
+   var0 = vars{k};
+   if isfield(anommap,var0)
+      d = size(anommap.(var0));
+      if length(d)==2
+         map.(var0) = anommap.(var0)+repmat(avmap.(var0)(:,iiav),[1,nrep]);
+      elseif length(d)==3
+         %keyboard;
+         map.(var0) = anommap.(var0)+repmat(avmap.(var0)(:,:,iiav),[1,1,nrep]);
+      end
+      map.err.(var0) = anommap.err.(var0);
+   end
+end
